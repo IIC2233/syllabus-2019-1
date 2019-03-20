0bace3ee3bfdaffcf70b7eca7da4d1e449dcc092
entamburini <entamburini@uc.cl>
2019-03-20 18:13:17 -0300

    Taller de apoyo: added files

---
+++ Ayudantias/Taller de apoyo/main.py
@@ -0,0 +1,61 @@
+# Actividad 07 2018-2
+
+# El objetivo es definir las siguientes clases y sus funciones según lo
+# entregado en el enunciado
+
+#######################################################################
+class Terreno:
+
+    def __init__(self, nombre):
+        """
+        Aca debes definir el constructor de la clase Terreno
+        """
+        
+
+
+
+class Ciudad:
+
+    def __init__(self, path):
+        """
+        Aca debes definir el constructor de la clase Ciudad
+        """
+        pass
+
+    def agregar_calle(self, origen, destino):
+        pass
+
+    def eliminar_calle(self, origen, destino):
+        pass
+
+    @property
+    def terrenos(self):
+        pass
+
+    @property
+    def calles(self):
+        pass
+
+    def verificar_ruta(self, ruta):
+        pass
+
+    def entregar_ruta(self, origen, destino):
+        pass
+
+
+    def ruta_corta(self, origen, destino):
+        pass
+
+
+    def ruta_entre_bombas(self, origen, *destinos):
+        pass
+
+    def ruta_corta_entre_bombas(self, origen, *destinos):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    pass
+    # Agrega aqui tus consultas
