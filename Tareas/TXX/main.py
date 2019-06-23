cd5674e5041b116255f97e61be152ced4c129052
Hernan Valdivieso <hfvaldivieso@uc.cl>
2019-06-23 19:36:41 -0400

    TXX :tada:

---
+++ Tareas/TXX/main.py
@@ -0,0 +1,15 @@
+from tree import AlgarroboTree
+from exceptions import NotFoundFile, InvalidTree, NotFoundValue
+from json import load
+
+
+def search(tree_path, value):
+    return []
+
+
+def build_and_save(data, output_path):
+    return None
+
+
+def route(data, start_value, end_value):
+    return []
---
+++ Tareas/TXX/tree.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right_child = None
+        self.left_child = None
+        # Puede agregar los atributos que quiera, pero debe usar
+        # los anteriores de todos modos
+
+
+class AlgarroboTree:
+    def __init__(self):
+        self.root = None
+        # Puede agregar los atributos que quiera, pero debe usar
+        # los anteriores de todos modos
+
+    def insert(self, value):
+        pass
+
+    def balance(self):
+        pass
+
+    # Puede agregar los métodos que quieras pero debes usar los 2 anteriores
